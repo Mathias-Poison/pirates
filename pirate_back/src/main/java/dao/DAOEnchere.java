bdc6ca1ccfa33f93fdaa31b25e2dfbf029a8c55c
Guillaume <guillaumevin-gv@hotmail.fr>
2023-02-22 09:51:41 +0100

    DAo enchere (Miss findallbycapitaine)

--- pirate_back/src/main/java/dao/DAOEnchere.java
+++ pirate_back/src/main/java/dao/DAOEnchere.java
@@ -1,5 +1,49 @@
 package dao;
 
-public class DAOEnchere {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import context.Singleton;
+import model.Enchere;
+
+public class DAOEnchere implements IDAOEnchere {
+	@Override
+	public Enchere findById(Integer id) {
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		Enchere enchere = em.find(Enchere.class,id);
+		em.close();
+		return enchere;
+	}
+	
+	@Override
+	public List<Enchere> findAll() {
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		
+		List<Enchere> encheres = em.createQuery("from Enchere").getResultList();
+		em.close();
+		return encheres;
+	}
+
+	@Override
+	public Enchere save(Enchere enchere) {
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		enchere = em.merge(enchere);
+		em.getTransaction().commit();
+		em.close();
+		return enchere;
+
+	}
+
+	@Override
+	public void delete(Integer id) {
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		Enchere enchere = em.find(Enchere.class,id);
+		em.getTransaction().begin();
+		em.remove(enchere);
+		em.getTransaction().commit();
+		em.close();	
+	}
 
 }
