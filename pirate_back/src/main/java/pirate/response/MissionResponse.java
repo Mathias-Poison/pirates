264e4641a921c35771a6bdfb15249e4777048fa7
Guillaume <guillaumevin-gv@hotmail.fr>
2023-03-14 11:39:53 +0100

    api.controller.mission + Exception, request,response...

---
+++ pirate_back/src/main/java/pirate/response/MissionResponse.java
@@ -0,0 +1,41 @@
+package pirate.response;
+
+public class MissionResponse {
+	
+	protected Integer id;
+	protected String libelle;
+	protected int duree;
+	protected String lieu;
+	protected int difficulte;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public int getDuree() {
+		return duree;
+	}
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+	public String getLieu() {
+		return lieu;
+	}
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+	public int getDifficulte() {
+		return difficulte;
+	}
+	public void setDifficulte(int difficulte) {
+		this.difficulte = difficulte;
+	}
+	
+}
