f608c0740a2d5f4869a57ee124f304686f79bc19
Guillaume <guillaumevin-gv@hotmail.fr>
2023-03-14 11:53:59 +0100

    views corrigé

--- pirate_back/src/main/java/pirate/controller/api/Views.java
+++ pirate_back/src/main/java/pirate/controller/api/Views.java
@@ -1,10 +1,7 @@
 package pirate.controller.api;
 
 public interface Views {
-	public class Bateau {
-
-	}
-
+	
 	public static interface Common {}
 	
 	public static interface Compte extends Common {}
