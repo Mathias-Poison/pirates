7799587a6a59e01d42b98bc76a404bc9896b3eea
Elodie Vanzeveren <elodie.vanzeveren@gmail.com>
2023-03-14 11:51:39 +0100

    add bateau

---
+++ pirate_back/src/main/java/pirate/request/BateauRequest.java
@@ -0,0 +1,50 @@
+package pirate.request;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import pirate.model.Armement;
+import pirate.model.Cargaison;
+import pirate.model.Mission;
+
+public class BateauRequest {
+	@NotBlank
+	private Integer id;
+	@NotBlank
+	private String nom;
+	private Cargaison cargaison;
+	private Armement armement;
+	private List<Mission> missions;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Cargaison getCargaison() {
+		return cargaison;
+	}
+	public void setCargaison(Cargaison cargaison) {
+		this.cargaison = cargaison;
+	}
+	public Armement getArmement() {
+		return armement;
+	}
+	public void setArmement(Armement armement) {
+		this.armement = armement;
+	}
+	public List<Mission> getMissions() {
+		return missions;
+	}
+	public void setMissions(List<Mission> missions) {
+		this.missions = missions;
+	}
+	
+
+}
