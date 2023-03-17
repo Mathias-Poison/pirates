89c31a527a7d04fd4145cd139daa3a5fc02e951c
mohammed <mohammed.moujahid1@gmail.com>
2023-03-17 15:32:25 +0100

    Impl Spring boot

---
+++ pirate_back/src/main/java/pirate/PirateSpringBootApplication.java
@@ -0,0 +1,12 @@
+package pirate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PirateSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PirateSpringBootApplication.class, args);
+	}
+}
