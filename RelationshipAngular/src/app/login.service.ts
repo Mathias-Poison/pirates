18bd72c8a59a3b231a46f5d66f1b93c25a46842e
mohammed <mohammed.moujahid1@gmail.com>
2023-04-04 01:40:29 +0200

    partie inscription finalisée

--- RelationshipAngular/src/app/login.service.ts
+++ RelationshipAngular/src/app/login.service.ts
@@ -1,27 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CompteHttpService } from './compte-http.service';
 import { Compte } from './models/models';
-import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private currentUserSubject: BehaviorSubject<Compte> = new BehaviorSubject<Compte>(null);
+  public connected: Observable<Compte> = this.currentUserSubject.asObservable();
+  constructor(private httpCompte : CompteHttpService) { }
 
-  compte:Compte | undefined;
-  connected: Compte | undefined;
-  constructor(private httpCompte : CompteHttpService, private router: Router) { }
+  authentifier(username:string, password:string) : Observable<Compte> {
+    return this.httpCompte.findByLoginAndPassword(username, password).pipe(map(compte => {
 
-  authentifier(login:string, password:string)  {
-    this.httpCompte.findByLoginAndPassword(login, password).subscribe((compte: Compte) => {
-      if(compte){
-        this.connected = compte;
-        this.router.navigate(['/pirate']);
-      }
-      else {
-        console.error("Erreur : compte introuvable. Veuillez vérifier votre login et votre mot de passe et réessayer.");
-      }
-      });
+      localStorage.setItem('connected', JSON.stringify(compte));
+      this.currentUserSubject.next(compte);
+      return compte;
+    }));
   }
 
 }
