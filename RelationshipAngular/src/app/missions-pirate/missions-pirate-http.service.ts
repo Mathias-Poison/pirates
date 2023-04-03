d20f0065ab39a9b5de575c106bcb44f41d074d00
Guillaume <guillaumevin-gv@hotmail.fr>
2023-04-03 17:38:34 +0200

    Début CRUD pour mission pirate

--- RelationshipAngular/src/app/missions-pirate/missions-pirate-http.service.ts
+++ RelationshipAngular/src/app/missions-pirate/missions-pirate-http.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Mission } from '../models/models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MissionsPirateHttpService {
+  missions : Array<Mission>= new Array<Mission>();
 
-  constructor() { }
+  constructor(private http : HttpClient) {
+  
+  }
+ 
+  findAll(): Array<Mission>{
+    return this.missions;
+  }
 }
--- RelationshipAngular/src/app/missions-pirate/missions-pirate.component.ts
+++ RelationshipAngular/src/app/missions-pirate/missions-pirate.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Mission } from '../models/models';
 
 @Component({
   selector: 'app-missions-pirate',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./missions-pirate.component.css']
 })
 export class MissionsPirateComponent {
+  
 
+  m1=new Mission(3,"Retrouver Maurice",2,"Chez Mathias",1,null,null,null)
+  missions: Array<Mission> = new Array<Mission>(this.m1);
 }
