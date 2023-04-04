92240caef9be779987d848f2e3d0b723d93ccdd7
Guillaume <guillaumevin-gv@hotmail.fr>
2023-04-04 15:48:57 +0200

    passation momo

--- RelationshipAngular/src/app/encherir/encherir.component.ts
+++ RelationshipAngular/src/app/encherir/encherir.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Mission } from '../models/models';
+import { MissionsPirateHttpService } from '../missions-pirate/missions-pirate-http.service';
 
 @Component({
   selector: 'app-encherir',
@@ -7,7 +9,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./encherir.component.css']
 })
 export class EncherirComponent {
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private missionHttpservice : MissionsPirateHttpService) {
+    this.route.params.subscribe(params => {
+      console.log(params);
     
+      });      
   }
+  mission= new Mission();
+
+  findById(id: number): Mission {
+    return this.missionHttpservice.missions.find(t => t.id == id);
+   }
 }
