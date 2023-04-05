7750619979707137b2209e286f75267ed1c543a6
mohammed <mohammed.moujahid1@gmail.com>
2023-04-05 16:08:06 +0200

    merge

---
+++ RelationshipAngular/src/app/missions-client/missions-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missions-client',
+  templateUrl: './missions-client.component.html',
+  styleUrls: ['./missions-client.component.css']
+})
+export class MissionsClientComponent {
+
+}
